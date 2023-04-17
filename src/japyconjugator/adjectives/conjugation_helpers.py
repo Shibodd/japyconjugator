60526e2842923057df4593add11bf8548f72133a
Shibodd <simonebondi456@gmail.com>
2023-04-17 13:51:00 +0200

    Adjective refactor

--- src/japyconjugator/adjectives/conjugation_helpers.py
+++ src/japyconjugator/adjectives/conjugation_helpers.py
@@ -1,25 +1,25 @@
 from ..defs import Polarity
 from .defs import AdjectiveClass
 
-def negative_invariant(adj_dictform, adj_class: AdjectiveClass):
+def invariant_with_suffix(adj_dictform, adj_class: AdjectiveClass, na_suffix = '', i_suffix = ''):
   if adj_class == AdjectiveClass.I:
-    return adj_dictform[:-1] + "くな"
+    return adj_dictform[:-1] + i_suffix
   else:
-    return adj_dictform + "じゃな"
-  
+    return adj_dictform + na_suffix
 
-def append_suffix_to_invariant(adj_dictform, adj_class: AdjectiveClass, polarity: Polarity,
-    affirmative_i_suffix = "",
-    affirmative_na_suffix = "",
-    negative_suffix = ""):
+def negative_invariant(adj_dictform, adj_class: AdjectiveClass):
+  return invariant_with_suffix(adj_dictform, adj_class, 
+    i_suffix = 'くな',
+    na_suffix = 'じゃな')
+
+def common_logic(adj_dictform, adj_class: AdjectiveClass, polarity: Polarity,
+    affirmative_i_suffix = '',
+    affirmative_na_suffix = '',
+    negative_suffix = ''):
   
   if polarity == Polarity.Affirmative:
-    invariant = adj_dictform[:-1]
-
-    if adj_class == AdjectiveClass.I:
-      return invariant + affirmative_i_suffix
-    else:
-      return invariant + affirmative_na_suffix
-    
+    return invariant_with_suffix(adj_dictform, adj_class,
+      i_suffix = affirmative_i_suffix,
+      na_suffix = affirmative_na_suffix)
   else:
     return negative_invariant(adj_dictform, adj_class) + negative_suffix
